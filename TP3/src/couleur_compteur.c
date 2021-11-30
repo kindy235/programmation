582a19b835923d6904430de5058b72c2ed63c956
kindy235 <bahaboubacar03@gmail.com>
2021-11-30 14:14:40 +0100

    v11

--- TP3/src/couleur_compteur.c
+++ TP3/src/couleur_compteur.c
@@ -3,54 +3,16 @@ Exercice 3.7 [★★★]
 
 Fichier : couleur_compteur.c
 Auteurs : Aboubacar BAH, Guth MOELLE
-Objectif : Rechercher par dichotomie un entier dans un tableau de 100 entier
+Objectif : Compteur les differentes couleurs distinctes et les afficher
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+// "couleurs.h" est un fichier header contenant la strcuture couleurs et les differentes fonctions associées.
+// Pour compiler : gcc -o couleur_compteur couleur_compteur.c couleurs.c
 #include "couleurs.h"
-
-// void colors_compteur(couleur colors[], int array_size)
-// {
-//     int i, j, once = 0, match = 0, matchancien = -1;
-//     for (i = 0; i < array_size; i++)
-//     {
-//         if (colors[i].compteur != 1)
-//         {
-//             printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//         }
-
-//         // for (j = 0; j < array_size; j++)
-//         // {
-//         //     // i != j && colors[i].rouge == colors[j].rouge && colors[i].vert == colors[j].vert && colors[i].bleu == colors[j].bleu && colors[i].alpha == colors[j].alpha && colors[i].compteur == colors[j].compteur
-//         //     if (i != j && colors[i].compteur != colors[j].compteur)
-//         //     {
-//         //         printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//         //     }
-//         // }
-//         // else
-//         // {
-//         //     printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//         //     matchancien = match;
-//         // }
-
-//         // if (colors[i].compteur != 1)
-//         // {
-//         //     printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//         // }
-//         // once = 1;
-//         //     break;
-//         // }
-//         // if (match != 0)
-//         // {
-//         //     once = 1;
-//         //     printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//         // }
-//         // printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, colors[i].rouge, colors[i].vert, colors[i].bleu, colors[i].alpha, colors[i].compteur);
-//     }
-// }
 
 int main()
 {
@@ -63,65 +25,61 @@ int main()
 
     // Declaration d'un tableaux de 100 couleurs
     couleur couleurs[100];
+
+    // On remplie le tableau de couleurs de manieres aleatoires avec la fonction "colors_fill"
     colors_fill(couleurs, 100);
 
-    // for (int k = 0; k < 20; k++)
-    // {
-    //     int r = rand() % 100;
-    //     couleurs[r].rouge = 0xff;
-    //     couleurs[r].vert = 0x23;
-    //     couleurs[r].bleu = 0x23;
-    //     couleurs[r].alpha = 0x45;
-    // }
-
-    colors_print(couleurs, 100);
-
-    int compteur[100] = {0};
-    int i, j, match;
-    for (i = 0; i < 100; i++)
+    // On definie trois couleurs qui vont se repeter de maniere aleatoires parmis les 100 couleurs
+    for (int k = 0; k < 100; k++)
     {
-        match = 1;
-        for (j = 0; j < 100; j++)
-        {
-            if (i != j && couleurs[i].rouge == couleurs[j].rouge && couleurs[i].vert == couleurs[j].vert && couleurs[i].bleu == couleurs[j].bleu && couleurs[i].alpha == couleurs[j].alpha)
-            {
-                match += 1;
-            }
-        }
-        compteur[i] = match;
-        printf("\n\nCouleur %d => 0x%x 0x%x 0x%x 0x%x %d", i + 1, couleurs[i].rouge, couleurs[i].vert, couleurs[i].bleu, couleurs[i].alpha, compteur[i]);
-        match = 0;
+        int i = rand() % 100;
+        int j = rand() % 100;
+        int s = rand() % 100;
+        couleurs[i].rouge = 0xff;
+        couleurs[i].vert = 0x23;
+        couleurs[i].bleu = 0x23;
+        couleurs[i].alpha = 0x45;
+
+        couleurs[j].rouge = 0xff;
+        couleurs[j].vert = 0x00;
+        couleurs[j].bleu = 0x23;
+        couleurs[j].alpha = 0x45;
+
+        couleurs[s].rouge = 0xaa;
+        couleurs[s].vert = 0x00;
+        couleurs[s].bleu = 0x22;
+        couleurs[s].alpha = 0x45;
     }
 
-    // int tab[100] = {0};
+    // On affiche les 100 couleurs à l'aide de la fonction "colors_print"
+    colors_print(couleurs, 100);
 
-    // for (int i = 0; i < 100; i++)
-    // {
+    // Comptage et affichage des couleurs semblables
+    // Compoteur[],tableau pour compteur le nombres d'apparition d'une couluers
+    int compteur[100] = {0};
+    // Verifie si une couleur a été dèjà compté ou pas
+    int doublon[100] = {0};
 
-    //     for (int j = 0; j < 100; j++)
-    //     {
-    //         if (tab[j] == 0)
-    //         {
-    //             tab[j] = couleurs[i].compteur;
-    //         }
-    //         if (tab[j] == couleurs[i].compteur)
-    //         {
-    //             break;
-    //         }
-    //     }
-    // }
+    int i, j;
+    for (i = 0; i < 100; i++)
+    {
+        if (doublon[i] == 0)
+        {
+            for (j = 0; j < 100; j++)
+            {
+                if (couleurs[i].rouge == couleurs[j].rouge && couleurs[i].vert == couleurs[j].vert && couleurs[i].bleu == couleurs[j].bleu && couleurs[i].alpha == couleurs[j].alpha)
+                {
+                    if (compteur[i] > 0)
+                    {
+                        doublon[j] = 1;
+                    }
+                    compteur[i] += 1;
+                }
+            }
+            printf("\n\n0x%x 0x%x 0x%x 0x%x %d", couleurs[i].rouge, couleurs[i].vert, couleurs[i].bleu, couleurs[i].alpha, compteur[i]);
+        }
+    }
 
-    // for (int i = 0; i < 100; i++)
-    // {
-    //     if (tab[i] != 0)
-    //     {
-    //         printf("\ni= %d\n", tab[i]);
-    //     }
-    // }
-
-    // colors_compteur(couleurs, 100);
-
-    // printf("\nCompteur de couleurs");
     printf("\n\n");
 
     return 0;
